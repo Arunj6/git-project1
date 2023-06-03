c147e4ab2832e88963cf28896d9f757102400b4b
arunj6 <arunjosephkochu619@gmail.com>
2023-06-03 12:00:40 +0530

    commit for ubuntu with root access

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,12 +2,5 @@
 
 #download node and npm
 sudo apt install nodejs
-
-#create our working directory if it doesnt exist
-DIR="/home/ubuntu/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+sudo :wq
+mkdir /home/ubuntu/express-app
